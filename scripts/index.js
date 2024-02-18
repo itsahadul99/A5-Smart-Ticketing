a76ae7dd7abf20d460140381af297798d5cc858a
itsahadul99 <itsahadul999@gmail.com>
2024-02-18 11:25:49 +0600

    complete grand total price

--- scripts/index.js
+++ scripts/index.js
@@ -18,16 +18,19 @@ for(const buttonTicket of buttonsTicket){
     buttonTicket.addEventListener('click', function(){
         const button = buttonTicket.innerText;
         setBackgroundColor(button)
+        // decrease the total seat number and show it
         const selectSeat = getElementById('select')
         selectSeat.style.color = '#1DD100';
         const seatLeft = getElementById('seat-left');
         const seatLeftNumber = parseInt(seatLeft.innerText);
         const seatLeftNumberLeft = seatLeftNumber - 1;
         seatLeft.innerText = seatLeftNumberLeft;
+        // increase selected seat and show it
         const selectSeatElement = getElementById('select-seat');
         const selectSeatNumber = parseInt(selectSeatElement.innerText);
         const selectSeatIncrease = selectSeatNumber + 1;
         selectSeatElement.innerText = selectSeatIncrease;
+        // show the selected ticket 
         const ticketName = getElementById('ticket-name');
         const p = document.createElement('p');
         p.innerText = buttonTicket.innerText;
@@ -40,10 +43,18 @@ for(const buttonTicket of buttonsTicket){
         const p3 = document.createElement('p');
         p3.innerText = ticketPrice;
         ticketPriceShower.appendChild(p3);
+        // total price calculation 
         const totalPriceElement = getElementById('total-price');
         const totalPriceNumber = parseInt(totalPriceElement.innerText);
         const totalTicketPrice = totalPriceNumber + ticketPrice;
         totalPriceElement.innerText = totalTicketPrice;
+
+        // grand total price calculation
+
+        const grandTotalElement = getElementById('grand-total');
+        const grandTotalNumber = parseInt(grandTotalElement.innerText);
+        const grandTotalPrice = grandTotalNumber + ticketPrice;
+        grandTotalElement.innerText = grandTotalPrice;
 
     })
 } 
